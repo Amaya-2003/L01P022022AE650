a81c94952f451f508038c309fee33e1db0f8addb
Amaya-2003 <caluchogamer@gmail.com>
2025-03-27 19:06:52 -0600

    Conexxion y builder program.cs

--- L01P022022AE650/Program.cs
+++ L01P022022AE650/Program.cs
@@ -1,7 +1,11 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<notasDBContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("notasDbConnection")));
 
 var app = builder.Build();
 
