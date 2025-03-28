f1bca8f06720f7a4c3d134ecf40699637815f46a
Amaya-2003 <caluchogamer@gmail.com>
2025-03-27 19:12:25 -0600

    tablas en clases

---
+++ L01P022022AE650/Models/departamentos.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+namespace L01P022022AE650.Models
+{
+    public class departamentos
+    {
+        [Key]
+        public int id { get; set; }
+        public string departamento { get; set; }
+    }
+}
